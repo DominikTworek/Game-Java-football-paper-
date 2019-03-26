3c354459363a3683fe8af755b3fb65f473a9f92e
DominikTworek <Dominikt123@gmail.com>
2019-03-26 20:22:18 +0100

    OneDot

--- src/game/PlayerController.java
+++ src/game/PlayerController.java
@@ -43,15 +43,6 @@ public class PlayerController implements Initializable {
     private Circle Point11;
 
     @FXML
-    private Circle point_20;
-
-    @FXML
-    private Circle point_30;
-
-    @FXML
-    private Circle point_40;
-
-    @FXML
     private Circle Point16;
 
     @FXML
@@ -61,15 +52,6 @@ public class PlayerController implements Initializable {
     private Circle Point12;
 
     @FXML
-    private Circle point20;
-
-    @FXML
-    private Circle point30;
-
-    @FXML
-    private Circle point40;
-
-    @FXML
     private Circle Point15;
 
     @FXML
@@ -77,6 +59,24 @@ public class PlayerController implements Initializable {
 
     @FXML
     private Circle Point13;
+
+    @FXML
+    private Circle Point21;
+
+    @FXML
+    private Circle Point22;
+
+    @FXML
+    private Circle Point23;
+
+    @FXML
+    private Circle Point24;
+
+    @FXML
+    private Circle Point25;
+
+    @FXML
+    private Circle Point26;
 
     //true - można
     //false - aktywne
@@ -91,6 +91,11 @@ public class PlayerController implements Initializable {
     private Boolean spr00_15;
     private Boolean spr00_16;
     private Boolean spr00_17;
+
+    private Boolean spr10_12;
+
+    private Boolean spr11_12;
+
     private Boolean spr12_10;
     private Boolean spr12_11;
     private Boolean spr12_19;
@@ -100,7 +105,20 @@ public class PlayerController implements Initializable {
     private Boolean spr12_14;
     private Boolean spr12_00;
 
+    private Boolean spr13_12;
 
+    private Boolean spr14_12;
+
+    private Boolean spr18_12;
+    private Boolean spr18_21;
+
+    private Boolean spr19_12;
+
+    private Boolean spr20_12;
+
+    private Boolean spr21_18;
+
+    private Boolean spr00;
     private Boolean spr10;
     private Boolean spr11;
     private Boolean spr12;
@@ -112,13 +130,24 @@ public class PlayerController implements Initializable {
     private Boolean spr18;
     private Boolean spr19;
     private Boolean spr20;
+    private Boolean spr21;
+    private Boolean spr22;
+    private Boolean spr23;
+    private Boolean spr24;
+    private Boolean spr25;
+    private Boolean spr26;
+    private Boolean spr27;
+    private Boolean spr28;
+    private Boolean spr29;
+    private Boolean spr30;
+    private Boolean spr31;
+    private Boolean spr32;
+    private Boolean spr33;
+    private Boolean spr34;
+    private Boolean spr35;
+    private Boolean spr36;
 
-    private Boolean sprStart;
 
-    @FXML
-    void ClickPoint20(MouseEvent event) {
-
-    }
 
     @FXML
     void ClickPoint30(MouseEvent event) {
@@ -140,83 +169,216 @@ public class PlayerController implements Initializable {
         mainGame.getChildren().add(line);
     }
 
-    void mainCheck(Boolean spr_laczenia, Boolean spr_laczenia2, Boolean aktywnosc, Boolean aktywnosc_po_nacisnieciu,
-                   Circle c1, Circle c2,
-                   Boolean c1spr, Boolean c2spr, Boolean c3spr, Boolean c4spr, Boolean c5spr, Boolean c6spr, Boolean c7spr, Boolean c8spr,
-                   Boolean ustawianie_laczenia1, Boolean ustawianie_laczenia2) {
-        if (!spr_laczenia && spr_laczenia2 && !aktywnosc) {
-            drawLine(c1, c2);
-            aktywnosc_po_nacisnieciu = false;
-            spr_laczenia = true;
-            spr_laczenia2 = true;
-            c1spr = true;
-            c2spr = true;
-            c3spr = true;
-            c4spr = true;
-            c5spr = true;
-            c6spr = true;
-            c7spr = true;
-            c8spr = true;
-            ustawianie_laczenia1 = true;
-            ustawianie_laczenia2 = true;
+    public void Point00Click(MouseEvent mouseEvent) {
+        if (!spr00_12 && !spr12_00 && !spr12) {
+            drawLine(Point00, Point12);
+            spr00 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr12 = true;
+            spr12_00 = true;
+            spr00_12 = true;
         }
     }
 
 
+
     @FXML
     void Point10Click(MouseEvent event) {
-        if (!spr00_10 && !sprStart) {
-            drawLine(Point00, Point10);
+        if (!spr10_12 && !spr12_10 && !spr12) {
+            drawLine(Point10, Point12);
             spr10 = false;
-            spr00_10 = true;
-        }
-        if (!spr12_10 && !spr12) {
-            drawLine(Point12, Point10);
-            spr10 = false;
+            spr00 = true;
+            spr16 = true;
+            spr17 = true;
+            spr36 = true;
+            spr27 = true;
+            spr28 = true;
+            spr11 = true;
+            spr12 = true;
+            spr10_12 = true;
             spr12_10 = true;
         }
     }
 
     @FXML
     void Point11Click(MouseEvent event) {
-        if (!spr00_11 && !sprStart) {
-            drawLine(Point00, Point11);
+        if (!spr11_12 && !spr12_11 && !spr12) {
+            drawLine(Point11, Point12);
             spr11 = false;
-            spr00_11 = true;
-        }
-        if (!spr12_11 && !spr12) {
-            drawLine(Point12, Point11);
-            spr11 = false;
+            spr19 = true;
+            spr18 = true;
+            spr00 = true;
+            spr10 = true;
+            spr27 = true;
+            spr28 = true;
+            spr29 = true;
+            spr12 = true;
+            spr11_12 = true;
             spr12_11 = true;
         }
     }
 
     @FXML
     void Point12Click(MouseEvent event) {
-        mainCheck(spr00_12, spr12_00, sprStart, spr12, Point00, Point12, spr10, spr11, spr19, spr18, spr20, spr13, spr14, sprStart, spr12_00, spr00_12);
+        if (!spr00_12 && !spr12_00 && !spr00) {
+            drawLine(Point00, Point12);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12_00 = true;
+            spr00_12 = true;
+        }
+        if (!spr12_18 && !spr18_12 && !spr18) {
+            drawLine(Point12, Point18);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12_18 = true;
+            spr18_12 = true;
+        }
+        if (!spr11_12 && !spr12_11 && !spr11) {
+            drawLine(Point11, Point12);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr11_12 = true;
+            spr12_11 = true;
+        }
+        if (!spr12_19 && !spr19_12 && !spr19) {
+            drawLine(Point11, Point19);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12_19 = true;
+            spr19_12 = true;
+        }
+        if (!spr12_20 && !spr20_12 && !spr20) {
+            drawLine(Point12, Point20);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12_20 = true;
+            spr20_12 = true;
+        }
+        if (!spr12_13 && !spr13_12 && !spr13) {
+            drawLine(Point12, Point13);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12_13 = true;
+            spr13_12 = true;
+        }
+        if (!spr12_14 && !spr14_12 && !spr14) {
+            drawLine(Point12, Point13);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12_14 = true;
+            spr14_12 = true;
+        }
+        if (!spr12_10 && !spr10_12 && !spr10) {
+            drawLine(Point12, Point10);
+            spr12 = false;
+            spr10 = true;
+            spr11 = true;
+            spr19 = true;
+            spr18 = true;
+            spr20 = true;
+            spr13 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12_10 = true;
+            spr10_12 = true;
+        }
     }
 
     @FXML
     void Point13Click(MouseEvent event) {
-        if (!spr00_13 && !sprStart) {
-            drawLine(Point00, Point13);
+        if (!spr13_12 && !spr12_13 && !spr12) {
+            drawLine(Point13, Point12);
             spr13 = false;
-            spr00_13 = true;
+            spr18 = true;
+            spr20 = true;
+            spr32 = true;
+            spr33 = true;
+            spr34 = true;
+            spr14 = true;
+            spr00 = true;
+            spr12 = true;
+            spr13_12 = true;
+            spr12_13 = true;
         }
     }
 
     @FXML
     void Point14Click(MouseEvent event) {
-        if (!spr00_14 && !sprStart) {
-            drawLine(Point00, Point14);
+        if (!spr14_12 && !spr12_14 && !spr12) {
+            drawLine(Point14, Point12);
             spr14 = false;
-            spr00_14 = true;
+            spr13 = true;
+            spr33 = true;
+            spr34 = true;
+            spr35 = true;
+            spr15 = true;
+            spr16 = true;
+            spr00 = true;
+            spr12 = true;
+            spr14_12 = true;
+            spr12_14 = true;
         }
     }
 
     @FXML
     void Point15Click(MouseEvent event) {
-        if (!spr00_15 && !sprStart) {
+        if (!spr00_15 && !spr00) {
             drawLine(Point00, Point15);
             spr15 = false;
             spr00_15 = true;
@@ -225,7 +387,7 @@ public class PlayerController implements Initializable {
 
     @FXML
     void Point16Click(MouseEvent event) {
-        if (!spr00_16 && !sprStart) {
+        if (!spr00_16 && !spr00) {
             drawLine(Point00, Point16);
             spr16 = false;
             spr00_16 = true;
@@ -234,16 +396,88 @@ public class PlayerController implements Initializable {
 
     @FXML
     void Point17Click(MouseEvent event) {
-        if (!spr00_17 && !sprStart) {
+        if (!spr00_17 && !spr00) {
             drawLine(Point00, Point17);
             spr17 = false;
             spr00_17 = true;
         }
     }
 
+    @FXML
+    public void Point18Click(MouseEvent mouseEvent) {
+        if (!spr12_18 && !spr18_12 && !spr12) {
+            drawLine(Point12, Point18);
+            spr18 = false;
+            spr11 = true;
+            spr19 = true;
+            spr22 = true;
+            spr21 = true;
+            spr23 = true;
+            spr20 = true;
+            spr13 = true;
+            spr12 = true;
+            spr12_18 = true;
+            spr18_12 = true;
+        }
+    }
+
+    @FXML
+    public void Point19Click(MouseEvent mouseEvent) {
+        if (!spr12_19 && !spr19_12 && !spr12) {
+            drawLine(Point12, Point19);
+            spr19 = false;
+            spr11 = true;
+            spr28 = true;
+            spr29 = true;
+            spr30 = true;
+            spr22 = true;
+            spr21 = true;
+            spr18 = true;
+            spr12 = true;
+            spr12_19 = true;
+            spr19_12 = true;
+        }
+    }
+
+    @FXML
+    public void Point20Click(MouseEvent mouseEvent) {
+        if (!spr12_20 && !spr20_12 && !spr12) {
+            drawLine(Point12, Point20);
+            spr20 = false;
+            spr18 = true;
+            spr21 = true;
+            spr23 = true;
+            spr31 = true;
+            spr32 = true;
+            spr33 = true;
+            spr13 = true;
+            spr12 = true;
+            spr12_20 = true;
+            spr20_12 = true;
+        }
+    }
+
+    @FXML
+    public void Point21Click(MouseEvent mouseEvent) {
+        if (!spr18_21 && !spr21_18 && !spr18) {
+            drawLine(Point18, Point21);
+            spr21 = false;
+            spr11 = true;
+            spr19 = true;
+            spr22 = true;
+            spr21 = true;
+            spr23 = true;
+            spr20 = true;
+            spr13 = true;
+            spr18 = true;
+            spr18_21 = true;
+            spr21_18 = true;
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        sprStart = false;
+        spr00 = false;
         spr10 = true;
         spr11 = true;
         spr12 = true;
@@ -260,6 +494,11 @@ public class PlayerController implements Initializable {
         spr00_15 = false;
         spr00_16 = false;
         spr00_17 = false;
+
+        spr10_12 = false;
+
+        spr11_12 = false;
+
         spr12_19 = false;
         spr12_18 = false;
         spr12_13 = false;
@@ -268,14 +507,19 @@ public class PlayerController implements Initializable {
         spr12_11 = false;
         spr12_14 = false;
         spr12_20 = false;
-    }
 
-    public void Point19Click(MouseEvent mouseEvent) {
-    }
+        spr13_12 = false;
 
-    public void Point18Click(MouseEvent mouseEvent) {
-    }
+        spr14_12 = false;
 
-    public void Point20Click(MouseEvent mouseEvent) {
+        spr18_12 = false;
+        spr18_21 = false;
+
+        spr19_12 = false;
+
+        spr20_12 = false;
+
+        spr21_18 = false;
+
     }
 }
